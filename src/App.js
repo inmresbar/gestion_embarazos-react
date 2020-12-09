9679258bf3b1cc3f2d5f2486855fd55ff721fe7e
inmresbar <56626645+inmresbar@users.noreply.github.com>
2020-12-09 08:02:22 +0100

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Signup from "./components/SignUp/Signup";
+import Signin from "./components/Signin";
+import Home from "./components/Home"
+import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
+import { Anchor } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Link } = Anchor;
+
+export default class App extends Component {
+    /*render() {
+        let navLink = (
+            <div className="Tab">
+                <NavLink exact to="/" activeClassName="activeLink" className="signUp">
+                    Sign Up
+                </NavLink>
+
+                <NavLink to="/sign-in" activeClassName="activeLink" className="Signin">
+                    Sign In
+                </NavLink>
+            </div>
+        );*/
+    render () {
+        return(
+        <Router>
+            <div className="App">
+                <ul>
+                    <li>
+                        <NavLink to="/" activeClassName="activeLink" className="signUp">Registro</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/sign-in" activaClassName="activeLink" className="signIn">Iniciar sesión</NavLink>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path="/" component={Signup}/>
+                </Switch>
+                <Switch>
+                    <Route exact path="/sign-in" component={Signin}/>
+                </Switch>
+                <Switch>
+                    <Route path="/home" component={Home}/>
+                </Switch>
+            </div>
+        </Router>
+        );
+    }
 }
-
-export default App;
